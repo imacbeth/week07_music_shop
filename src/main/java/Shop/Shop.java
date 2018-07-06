9d0cda7b05fe6fd63ac35fc82466e78316b765d9
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-06 16:04:05 +0100

    shop getter methods pass tests

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -7,12 +7,40 @@ import java.util.ArrayList;
 public class Shop {
 
 
-    ArrayList<ISell> stock;
+    ArrayList<StockItem> stock;
     private String name;
+    private double till;
 
-    public Shop(ArrayList<ISell> stock, String name) {
+
+    public Shop(ArrayList<StockItem> stock, String name) {
         this.stock = stock;
         this.name = name;
+        this.till = 0.00;
+    }
+
+    public ArrayList<StockItem> getStock() {
+        return stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void addStock(StockItem stockItem) {
+         stock.add(stockItem);
+    }
+
+    public void sellStock(StockItem stockItem) {
+        stock.remove(stockItem);
+        till += stockItem.getSellPrice();
     }
 
 
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,9 +1,35 @@
 import Behaviours.ISell;
+import Shop.Shop;
+import Shop.StockItem;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+    Shop shop;
+    ArrayList<StockItem> stock;
 
+    @Before
+    public void setUp(){
+        stock = new ArrayList<StockItem>();
+        shop = new Shop(stock, "Ray's Music Exchange");
+    }
 
+    @Test
+    public void hasName() {
+        assertEquals("Ray's Music Exchange", shop.getName());
+    }
 
+    @Test
+    public void hasStock() {
+        assertEquals(stock, shop.getStock());
+    }
+
+    @Test
+    public void canGetTill(){
+        assertEquals(0.00, shop.getTill(), 0.01);
+    }
 }
