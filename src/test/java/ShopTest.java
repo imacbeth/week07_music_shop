8cae90a9cf4406d4acbb36f8580b8804bde07117
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-06 16:17:46 +0100

    shop add stock and sell stock methods pass test

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,6 +1,7 @@
 import Behaviours.ISell;
-import Shop.Shop;
+import Shop.*;
 import Shop.StockItem;
+import Shop.Guitar;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +12,12 @@ import static org.junit.Assert.assertEquals;
 public class ShopTest {
     Shop shop;
     ArrayList<StockItem> stock;
+    Guitar guitar;
 
     @Before
     public void setUp(){
         stock = new ArrayList<StockItem>();
+        guitar = new Guitar(115, 250, InstrumentType.STRING,6, "Fender");
         shop = new Shop(stock, "Ray's Music Exchange");
     }
 
@@ -31,5 +34,18 @@ public class ShopTest {
     @Test
     public void canGetTill(){
         assertEquals(0.00, shop.getTill(), 0.01);
+    }
+
+    @Test
+    public void canAddStock(){
+        shop.addStock(guitar);
+        assertEquals(guitar, shop.getStock().get(0));
+    }
+
+    @Test
+    public void canSellStock(){
+        shop.addStock(guitar);
+        shop.sellStock(guitar);
+        assertEquals(250, shop.getTill(), 0.01);
     }
 }
